7cb0cf84f5b9a738ec27d81535712ee8f5ddb9f5
jakub wolodzko <s175634@o365.student.pg.edu.pl>
2022-12-14 10:36:37 +0100

    added date to network interrupt script

--- scripts/network-interrupt.sh
+++ scripts/network-interrupt.sh
@@ -1,21 +1,26 @@
 echo "Simulating network interruption"
+TIME=$(TZ='Europe/Warsaw' date)
 for (( c=1; c<=100; c++ ))
 do 
     if [ $((c%2)) -eq 0 ]
     then
     echo "--- Turning OFF network in BEATS for 5 seconds ---"
-    docker network disconnect elk_elk beats
+    echo "--- TIME: ${TIME}"
+    docker network disconnect elk_elk beats 
     docker inspect beats -f "{{json .NetworkSettings.Networks }}"
     sleep 5
     echo "--- Turning ON network in BEATS. ---"
+    echo "--- TIME: ${TIME}"
     docker network connect elk_elk beats
     docker inspect beats -f "{{json .NetworkSettings.Networks }}"
     else
     echo "--- Turning OFF network in LOGSTASH for 5 seconds ---"
+    echo "--- TIME: ${TIME}"
     docker network disconnect elk_elk logstash
     docker inspect logstash -f "{{json .NetworkSettings.Networks }}"
     sleep 5
     echo "--- Turning ON network in LOGSTASH. ---"
+    echo "--- TIME: ${TIME}"
     docker network connect elk_elk logstash
     docker inspect logstash -f "{{json .NetworkSettings.Networks }}"
     fi
